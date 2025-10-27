4e4a8ff9863cf638f11ea5752c905598b9c042a4
juhyun <hanaheart030@gmail.com>
2025-10-27 14:29:42 +0900

    feat(racingcar): implement RacingGame to control race flow

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -6,15 +6,7 @@ import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
-        List<String> carNames = InputView.readCarNames();
-        int tryCount = InputView.readTryCount();
-
-        //임시 테스트
-        Cars cars = new Cars(carNames);
-
-        for (int i = 0; i < tryCount; i++) {
-            cars.moveAll();
-            cars.printStatus();
-        }
+        RacingGame racingGame = new RacingGame();
+        racingGame.start();
     }
 }
---
+++ src/main/java/racingcar/RacingGame.java
@@ -0,0 +1,23 @@
+package racingcar;
+
+import java.util.List;
+
+public class RacingGame {
+    public void start() {
+        // 입력 받기
+        List<String> carNames = InputView.readCarNames();
+        int tryCount = InputView.readTryCount();
+
+        // 자동차 리스트 초기화
+        Cars cars = new Cars(carNames);
+
+        // 실행 결과 안내
+        System.out.println("\n실행 결과");
+
+        // 시도 횟수만큼 경주 반복
+        for (int i = 0; i < tryCount; i++) {
+            cars.moveAll();
+            cars.printStatus();
+        }
+    }
+}
